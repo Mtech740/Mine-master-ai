601913627fbe8935df1edd630461ad65bf179e82
Mumba Sampa <mumbasampa3@gmail.com>
2025-05-24 12:01:09 +0200

    Save changes before pull

--- script.js
+++ script.js
@@ -11,18 +11,24 @@ function resetGrid() {
     grid.appendChild(tile);
   }
 }
+
 function toggleTile(index, tile) {
   board[index] = board[index] ? 0 : 1;
   tile.classList.toggle("clicked");
 }
+
 async function predict() {
   const input = tf.tensor([board]);
   const model = await tf.loadLayersModel("model.json");
   const prediction = model.predict(input).dataSync();
   const tiles = document.getElementsByClassName("tile");
   for (let i = 0; i < 25; i++) {
-    if (prediction[i] > 0.5) tiles[i].classList.add("safe");
-    else tiles[i].classList.add("mine");
+    if (prediction[i] > 0.5) {
+      tiles[i].classList.add("safe");
+    } else {
+      tiles[i].classList.add("mine");
+    }
   }
 }
+
 resetGrid();
